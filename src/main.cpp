1d0b67dcb5933edc4fb3f560b8164c0dab9ce04b
Adel0s <adel.drinceanu@gmail.com>
2024-11-19 21:57:13 +0200

    modify main

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,17 @@
-#include <circle.hpp>
-#include <iostream>
+#include "circle.hpp"
 
 int main() {
-    // Cream un obiect Circle
-    Circle circle(5.0);
-    // Afisam aria și diametrul cercului
-    std::cout << "Circle with radius " << circle.getRadius() << " has:" << std::endl;
-    std::cout << "Area: " << circle.calculateArea() << std::endl;
-    std::cout << "Diameter: " << circle.calculateDiameter() << std::endl;
+    Circle c1(5.0); // Constructor cu parametru
+    Circle c2;      // Constructor implicit
 
-    std::cout << std::endl;
+    c2 = c1;        // Operator de atribuire
+    Circle c3 = c1; // Constructor de copiere
 
-    Circle circle2(-5);
-    // Afisam aria și diametrul cercului initializat cu raza negativa
-    std::cout << "Circle with radius " << circle2.getRadius() << " has:" << std::endl;
-    std::cout << "Area: " << circle2.calculateArea() << std::endl;
-    std::cout << "Diameter: " << circle2.calculateDiameter() << std::endl;
+    c1 = c1;        // Test atribuirea la sine
 
-    std::cout << std::endl;
+    std::cout << "c1 radius: " << c1.getRadius() << ", area: " << c1.calculateArea() << ", diameter: " << c1.calculateDiameter() << "\n";
+    std::cout << "c2 radius: " << c2.getRadius() << ", area: " << c2.calculateArea() << ", diameter: " << c2.calculateDiameter() << "\n";
+    std::cout << "c3 radius: " << c3.getRadius() << ", area: " << c3.calculateArea() << ", diameter: " << c3.calculateDiameter() << "\n";
 
-    // Incercam copierea (aceste linii ar trebui sa fie comentate pentru a compila cu succes)
-    // Circle circleCopy = circle;       // Eroare: constructorul de copiere este dezactivat, pentru activare voi comenta linia din fisierul header circle.hpp
-    // std::cout << "Circle with radius " << circleCopy.getRadius() << " has:" << std::endl;
-    // std::cout << "Area: " << circleCopy.calculateArea() << std::endl;
-    // std::cout << "Diameter: " << circleCopy.calculateDiameter() << std::endl;
-    // Circle anotherCircle(10.0);
-    // anotherCircle = circle;           // Eroare: operatorul de atribuire este dezactivat
-
-    // Demonstrarea apelarii move constructorului default
-    Circle tempCircle(10.0);
-    tempCircle = std::move(circle); // Mutam resursele de la circle in tempCircle
-
-    Circle circleImplicit;
-    std::cout << "Implicit constructor:\nCircle radius for implicit constructor: " << circleImplicit.getRadius() << std::endl;
     return 0;
 }

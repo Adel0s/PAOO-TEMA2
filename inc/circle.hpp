32578b2e658d116bbc8222be640d10e501996283
Adel0s <adel.drinceanu@gmail.com>
2024-11-19 20:49:02 +0200

    activate back copy constructor and assigment operator

--- inc/circle.hpp
+++ inc/circle.hpp
@@ -15,8 +15,8 @@ public:
     // Dezactivam constructorul de copiere si operatorul de atribuire
     // Pentru a arata ca aceste metode se genereaza in mod automat o sa comentez linia construcotrului de copiere(anulez dezactivarea)
     // In aces caz se genereaza automat constructorul de copiere si voi exemplifica in main.cpp acest lucru
-    Circle(const Circle&) = delete;
-    // Circle& operator=(const Circle&) = delete;
+    Circle(const Circle&);
+    Circle& operator=(const Circle&);
 
     // Metode pentru calculul ariei si diametrului
     double calculateArea() const;
